36915fa4ec4a19d71f8f8004a9d6180e0a1b4059
David Jinkins <david.jinkins@gmail.com>
2012-04-01 20:59:49 -0400

    about to change to exact gamma v calculation

--- likEM.m
+++ likEM.m
@@ -9,9 +9,9 @@ cd '/gpfs/home/dcj138/work/ConspCons/'
 load dat
 
 %open parallel functionality
-%if matlabpool('size')<12
-%    matlabpool open 12
-%end
+if matlabpool('size')<12
+    matlabpool open 12
+end
 
 %open diary (NOW USING LOG FILES called in matlab command line when opening)
 diary 3-31-12-2.txt
@@ -88,7 +88,7 @@ ot_ind = randi(29,size(c,1),1);
 ot_ind_old = ones(size(c,1),1);
 
 %make c of a managable size
-I = randi(size(c,1),1000,1);
+I = randi(size(c,1),10000,1);
 %load I;
 c = c(I,:);
 ot_ind = ot_ind(I);
@@ -102,8 +102,8 @@ y = c.y;
 type = c.type;
 
 %init = [1;7.59210648140400;4.73543180019105;5.74862875334654;4.95440784574850;12.7037395527745;5.69478318955006;7.88753934896234;1.44938461037568;9.18538421369885;9.05132228657440;9.00566247190964;3.49072874128893;8.27878887702105;10.4018645159034;8.17370675069044;15.4762574500119;9.68697383660247;3.55478951284719;7.94333412618397;7.18239264327919;11.4795764405883;4.45354347075682;9.18123307895688;4.89033935794127;12.1057696447419;9.14687789130620;9.31117750145790;7.41878093944565;7.61952386298256;7.70619388603037;8.23975939816500;7.71149476704346;7.84473732613889;7.25724006498097;7.48925368453039;7.50087264872762;7.82344615001618;7.60552520095296;7.57603338714131;7.19619527149678;7.87225119947359;7.39836937524840;7.30019148841177;7.81848287278297;7.73258241738970;9.12571047476629;7.32590778396217;7.73716194637371;8.15061049349470;7.69021491224891;7.67405438242449;7.66479184955140;7.72149575366749;6.55816617452169;7.71298023499229;7.98213121498031;7.23763544816502;0.438919082280316;];
-%init = linspace(0,1,36)';
-init = 0;
+init = linspace(0,1,36)';
+%init = 0;
 %scale
 %scl = abs(likEM_loop(init,cons,expend,y,price,ot_ind,w_b,c_b,1));
 scl = 1;
@@ -115,24 +115,25 @@ while min(ot_ind == ot_ind_old) == 0
 % E step
 
 %options=optimset('Display','iter','Algorithm','interior-point','TolCon',1e-3,'TolX',1e-4,'TolFun',1e-3,'MaxFunEvals',1e5,'MaxIter',20);%,'SubProblemAlgorithm','cg');%,'Hessian','bfgs');
-options=optimset('Display','iter','LargeScale','off','TolCon',1e-6,'TolX',1e-8,'TolFun',1e-6,'MaxFunEvals',1e5,'MaxIter',20,'HessUpdate','bfgs');%,'SubProblemAlgorithm','cg');%,'Hessian','bfgs');
-%options = gaoptimset('Display','iter','InitialPopulation',init,'PopulationSize',36,'UseParallel','always','PlotFcns',@gaplotbestf,'StallGenLimit',5);
+%options=optimset('Display','iter','LargeScale','off','TolCon',1e-12,'TolX',1e-12,'TolFun',1e-10,'MaxFunEvals',1e5,'MaxIter',150,'HessUpdate','bfgs');%,'SubProblemAlgorithm','cg');%,'Hessian','bfgs');
+options = gaoptimset('Display','iter','InitialPopulation',init,'PopulationSize',36,'UseParallel','always','PlotFcns',@gaplotbestf,'StallGenLimit',20);
 
-[X,fval,exitflag] = fminsearch(@(x) likEM_loop(x,cons,expend,y,price,ot_ind,w_b,c_b,scl,v),init,options);
-%X = ga(@(x) likEM_loop(x,cons,expend,y,price,ot_ind,w_b,c_b,scl),1,[],[],[],[],1e-12,1-1e-12,[],[],options);   
+%[X,fval,exitflag] = fminunc(@(x) likEM_loop(x,cons,expend,y,price,ot_ind,w_b,c_b,scl,v),init,options);
+X = ga(@(x) likEM_loop(x,cons,expend,y,price,ot_ind,w_b,c_b,scl),1,[],[],[],[],1e-12,1-1e-12,[],[],options);   
 
 [~,mu,sig,zm] = likEM_loop(X,cons,expend,y,price,ot_ind,w_b,c_b,scl);
 
-init = X;
-implied_alp = exp(X)/(1+exp(X));
-%implied_alp = X;
-display(implied_alp);
+%init = X;
+%implied_alp = exp(X)/(1+exp(X));
+implied_alp = X;
+
 display([mu,sig,zm]);
+display(implied_alp);
 
 % M step
 
 ot_ind_old = ot_ind;
-ot_ind = not_ind(init,cons,expend,y,type,price,w_b,c_b,v,mu,sig,zm);
+ot_ind = not_ind(X,cons,expend,y,type,price,w_b,c_b,v,mu,sig,zm);
 
 %if toc>2*3600
 %   break; 
--- likEM_loop.m
+++ likEM_loop.m
@@ -1,52 +1,52 @@
 function [lik,mu,sig,zm] = likEM_loop(inp,cons,expend,y,price,ot_ind,w_b,c_b,scl,v)
 %Calculates likelihood given type and weight of conspicuous consumption.  The coefficient on food at home is set to one, and all approximations are around one.
 
-%alp = inp;
-alp = exp(inp(1))/(1+exp(inp(1)));
+alp = inp;
+%alp = exp(inp(1))/(1+exp(inp(1)));
 %zm  = exp(inp(2:end))./(1+exp(inp(2:end)));
 
 %get the equilibrium c's for a wealth grid 
 b = logical(1-eye(29));
-g = @(x,K,k,m,bet) (1-alp)*sum(bet(b(:,k)))/(bet(k) + alp*(sum(bet(b(:,k)))))*x + K*x.^(bet(k)/(alp*(sum(bet(b(:,k)))))); 
-g_w =  @(x,K,k,m,bet) g(x,K,k,m,bet) + price(m,k)*x;
-g_p = @(x,K,k,m,bet) 1/alp*((1-alp)*price(m,k) - bet(k)/(sum(bet(b(:,k))))*g(x,K,k,m,bet)./x);
-g_pp = @(x,K,k,m,bet) -bet(k)/(alp*(sum(bet(b(:,k)))))*(g_p(x,K,k,m,bet)./x-g(x,K,k,m,bet)./(x.^2));
-
-%Create constant and wealth/consumption grids (for ease of finding c given
-%W)
-
-K = cell(29,18);
-cg = cell(29,18);
-wg = cell(29,18);
-for m = 1:18
-    for k=1:29 
-        %K{k,m} = @(bet) (w_b(m)-((sum(bet(b(:,k)))-bet(k))*(1-alp)/(bet(k)+alp*(sum(bet(b(:,k)))-bet(k)))+price(m,k))*bet(k)/price(m,k)*w_b(m))/(bet(k)/price(m,k)*w_b(m))^(bet(k)/((sum(bet(b(:,k)))-bet(k))*alp));   
-        K{k,m} = @(bet) (bet(k)/(price(m,k)*sum(bet))*w_b(m))^(1-bet(k)/(alp*sum(bet(b(:,k)))))*(1-sum(bet(b(:,k)))*(1-alp)/(bet(k)+alp*sum(bet(b(:,k)))));
-        cg{k,m} = linspace(1e-12,c_b(m)/price(m,k),1000);
-        wg{k,m} = @(bet) g_w(cg{k,m}/price(m,k),K{k,m}(bet),k,m,bet);
-    end
-end
-
-%create dgdc as a function of other gammas, for not food at home
-dgdc = cell(29,18);
-for m = 1:18
-    for k=1:29     
-        %dgdc{k,m} = @(x,gam,W,k,m) gam(k)./x + alp * x * sum(gam(b(:,k))) * g_p(x,K{k,m}(gam),k,m,gam).^2/g(x,K{k,m}(gam),k,m,gam).^2 ...
-        %    -alp * x * sum(gam(b(:,k))) * g_pp(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam) ...
-        %    +price(m,k).^2 * x ./((W - price(m,k) * x).^2 );
-        dgdc{k,m} = @(x,gam,W,k,m) (1/x)/(gam(k)/x^2 - alp * sum(gam(b(:,k)))*g_pp(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam)...
-            + alp * sum(gam(b(:,k)))*g_p(x,K{k,m}(gam),k,m,gam)^2/g(x,K{k,m}(gam),k,m,gam)^2 + price(m,k)^2/(W-price(m,k)*x)^2);
-    end
-end
-
-%create dhdc for not food at home
-dhdc = cell(29,18);
-b = logical(1-eye(29));
-for m = 1:18
-    for k=1:29     
-        dhdc{k,m} = @(x,gam,W,k,m) (gam(k)./x + price(m,k).^2 ./g(x,K{k,m}(gam),k,m,gam).^2) * g(x,K{k,m}(gam),k,m,gam)./(alp * g_p(x,K{k,m}(gam),k,m,gam)) + sum(gam(b(:,k))) * (g_p(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam) - g_pp(x,K{k,m}(gam),k,m,gam)/g_p(x,K{k,m}(gam),k,m,gam));
-    end
-end
+% g = @(x,K,k,m,bet) (1-alp)*sum(bet(b(:,k)))/(bet(k) + alp*(sum(bet(b(:,k)))))*x + K*x.^(bet(k)/(alp*(sum(bet(b(:,k)))))); 
+% g_w =  @(x,K,k,m,bet) g(x,K,k,m,bet) + price(m,k)*x;
+% g_p = @(x,K,k,m,bet) 1/alp*((1-alp)*price(m,k) - bet(k)/(sum(bet(b(:,k))))*g(x,K,k,m,bet)./x);
+% g_pp = @(x,K,k,m,bet) -bet(k)/(alp*(sum(bet(b(:,k)))))*(g_p(x,K,k,m,bet)./x-g(x,K,k,m,bet)./(x.^2));
+% 
+% %Create constant and wealth/consumption grids (for ease of finding c given
+% %W)
+% 
+% K = cell(29,18);
+% cg = cell(29,18);
+% wg = cell(29,18);
+% for m = 1:18
+%     for k=1:29 
+%         %K{k,m} = @(bet) (w_b(m)-((sum(bet(b(:,k))))*(1-alp)/(bet(k)+alp*(sum(bet(b(:,k))))+price(m,k))*bet(k)/price(m,k)*w_b(m))/(bet(k)/price(m,k)*w_b(m))^(bet(k)/((sum(bet(b(:,k))))*alp));   
+%         K{k,m} = @(bet) (bet(k)/(price(m,k)*sum(bet))*w_b(m))^(1-bet(k)/(alp*sum(bet(b(:,k)))))*(1-sum(bet(b(:,k)))*(1-alp)/(bet(k)+alp*sum(bet(b(:,k)))));
+%         cg{k,m} = linspace(1e-12,c_b(m)/price(m,k),1000);
+%         wg{k,m} = @(bet) g_w(cg{k,m}/price(m,k),K{k,m}(bet),k,m,bet);
+%     end
+% end
+% 
+% %create dgdc as a function of other gammas, for not food at home
+% dgdc = cell(29,18);
+% for m = 1:18
+%     for k=1:29     
+%         %dgdc{k,m} = @(x,gam,W,k,m) gam(k)./x + alp * x * sum(gam(b(:,k))) * g_p(x,K{k,m}(gam),k,m,gam).^2/g(x,K{k,m}(gam),k,m,gam).^2 ...
+%         %    -alp * x * sum(gam(b(:,k))) * g_pp(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam) ...
+%         %    +price(m,k).^2 * x ./((W - price(m,k) * x).^2 );
+%         dgdc{k,m} = @(x,gam,W,k,m) (1/x)/(gam(k)/x^2 - alp * sum(gam(b(:,k)))*g_pp(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam)...
+%             + alp * sum(gam(b(:,k)))*g_p(x,K{k,m}(gam),k,m,gam)^2/g(x,K{k,m}(gam),k,m,gam)^2 + price(m,k)^2/(W-price(m,k)*x)^2);
+%     end
+% end
+% 
+% %create dhdc for not food at home
+% dhdc = cell(29,18);
+% b = logical(1-eye(29));
+% for m = 1:18
+%     for k=1:29     
+%         dhdc{k,m} = @(x,gam,W,k,m) (gam(k)./x + price(m,k).^2 ./g(x,K{k,m}(gam),k,m,gam).^2) * g(x,K{k,m}(gam),k,m,gam)./(alp * g_p(x,K{k,m}(gam),k,m,gam)) + sum(gam(b(:,k))) * (g_p(x,K{k,m}(gam),k,m,gam)/g(x,K{k,m}(gam),k,m,gam) - g_pp(x,K{k,m}(gam),k,m,gam)/g_p(x,K{k,m}(gam),k,m,gam));
+%     end
+% end
 
 %% calculate likelihood
 %ot is observation type, should have one for each household
@@ -61,17 +61,25 @@ for h = 1:size(expend,1)
     if ot ~= 1
         gam = cons(h,:)/cons(h,1);
         %gam(ot) = 1;
-        tempw = wg{ot,y(h)}(gam);
-    	ind = find(expend(h)>tempw,1,'last');
-    	ogam = gam(ot) + dgdc{ot,y(h)}(cg{ot,y(h)}(ind)/price(y(h),ot),gam,tempw(ind),ot,y(h))*(cons(h,ot)-cg{ot,y(h)}(ind))/price(y(h),ot);
-        gam(ot) = max(ogam,0);
+        %tempw = wg{ot,y(h)}(gam);
+    	%ind = find(expend(h)>tempw,1,'last');
+        %ogam = gam(ot) + dgdc{ot,y(h)}(cg{ot,y(h)}(ind)/price(y(h),ot),gam,tempw(ind),ot,y(h))*(cons(h,ot)-cg{ot,y(h)}(ind))/price(y(h),ot);
+        %gam(ot) = max(ogam,0);
+        gam(ot) = sum(gam(b(:,ot)))*((1-alp)/(expend(h)/(cons(h,ot)*price(y(h),ot))-1)-alp);
+        if gam(ot)<0
+            gam(ot) = 50; %punishment, give a large enough value to hurt, but not inf which kills likelihood
+        end
         ngam(:,h) = gam;
     else
         gam = cons(h,:)/cons(h,1);
-        tempw = wg{ot,y(h)}(gam);
-        ind = find(expend(h)>tempw,1,'last');
-        hgam = sum(gam(2:end)) + dhdc{ot,y(h)}(cg{ot,y(h)}(ind)/price(y(h),ot),gam,tempw(ind),ot,y(h))*(cons(h,ot)-cg{ot,y(h)}(ind))/price(y(h),ot);
-        hat = max(hgam,0);
+        %tempw = wg{ot,y(h)}(gam);
+        %ind = find(expend(h)>tempw,1,'last');
+        %hgam = sum(gam(2:end)) + dhdc{ot,y(h)}(cg{ot,y(h)}(ind)/price(y(h),ot),gam,tempw(ind),ot,y(h))*(cons(h,ot)-cg{ot,y(h)}(ind))/price(y(h),ot);
+        %hat = max(hgam,0);
+        hat = ((1-alp)/(expend(h)/(cons(h,ot)*price(y(h),ot))-1)-alp)^-1;
+        if hat < 0
+            hat = 500; %punishment, large enough to hurt but not kill
+        end
         gam(2:end) = cons(h,2:end)*hat/(expend(h)-cons(h,ot));
         ngam(:,h) = gam;
     end
